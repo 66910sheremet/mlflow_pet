b27d6fde93d40ffea2250bbdd0725c4ec91c7a91
66910 <669101mac@gmail.com>
2024-07-01 18:47:01 +0300

    retrieve

--- mlflow_utils.py
+++ mlflow_utils.py
@@ -2,9 +2,14 @@
 import mlflow
 from typing import Any
 
+import pandas as pd
+
+from sklearn.datasets import make_classification
+
 
 def create_mlflow_experiment(
-    experiment_name: str, artifact_location: str, tags: dict[str, Any]) -> str:
+    experiment_name: str, artifact_location: str, tags: dict[str, Any]
+) -> str:
     """
     Create a new mlflow experiment with the given name and artifact location.
 
@@ -30,4 +35,33 @@ def create_mlflow_experiment(
         print(f"Experiment {experiment_name} already exists.")
         experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
+    mlflow.set_experiment(experiment_name=experiment_name)
+
     return experiment_id
+
+
+def get_mlflow_experiment(
+    experiment_id: str = None, experiment_name: str = None
+) -> mlflow.entities.Experiment:
+    """
+    Retrieve the mlflow experiment with the given id or name.
+
+    Parameters:
+    ----------
+    experiment_id: str
+        The id of the experiment to retrieve.
+    experiment_name: str
+        The name of the experiment to retrieve.
+
+    Returns:
+    -------
+    experiment: mlflow.entities.Experiment
+        The mlflow experiment with the given id or name.
+    """
+    if experiment_id is not None:
+        experiment = mlflow.get_experiment(experiment_id)
+    elif experiment_name is not None:
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+    else:
+        raise ValueError("Either experiment_id or experiment_name must be provided.")
+    return experiment
